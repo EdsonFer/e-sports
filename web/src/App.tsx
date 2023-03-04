6889df6aed9391f7f93f677227b2999c10cea056
edson.fernandes <edlivelifehd1z@gmail.com>
2023-03-04 18:43:09 -0300

    background logo e imgs

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,42 @@
+import "./styles/main.css";
+
+import logoImg from "./assets/Logo.svg";
+
 function App() {
-  return <h1>hello world</h1>;
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="logo" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        seu{" "}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          Duo
+        </span>{" "}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="">
+          <img src="/image 1.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image 2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image 3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image 5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image 6.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/image 7.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default App;
